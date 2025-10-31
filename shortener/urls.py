0c18e6e55600e93247285680f671deeeb83c3137
Sean Chua <88256324+SeansC12@users.noreply.github.com>
2025-10-31 15:22:53 +0800

    Further simplify codebase

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "url_shortener"  # Changed from 'shortener' to match your app name
-
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:alias>/", views.redirect_to_original, name="redirect"),
